99c3210e7744a8356fbc364f6b041f07d835be5b
Priyanshu Nandan <priyanshunandan_ee22b17_49@dtu.ac.in>
2025-04-15 18:52:16 +0530

    Added git-router-dom for different page Routing for a SPA

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Blog from './Blog';
+
 function App() {
   return (
-<>
+    <BrowserRouter>
 <Nav/>
-</>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+</BrowserRouter>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,9 @@
 import React, {useState} from "react";
 import './Nav.css';
+import { Link, useLocation } from 'react-router-dom';
 
 function Nav(props){
+    const location = useLocation();
     const [bold, setBold] = useState({fontWeight:400});
     return(
     <div className="nav">
@@ -11,11 +13,10 @@ function Nav(props){
 
             <div className="menu">
             <ul>
-                <li><a href='#' style={bold}>Home</a></li>
-                
-                <li><a href='#'>Projects</a></li>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Blog</a></li>
+                <li><Link to='/' style={location.pathname === '/' ? {fontWeight:700} : {fontWeight:400}}>Home</Link></li>
+                <li><Link to='/projects' style={location.pathname === '/projects' ? {fontWeight:700} : {fontWeight:400}}>Projects</Link></li>
+                <li><Link to='/about' style={location.pathname === '/about' ? {fontWeight:700} : {fontWeight:400}}>About</Link></li>
+                <li><Link to='/blog' style={location.pathname === '/blog' ? {fontWeight:700} : {fontWeight:400}}>Blog</Link></li>
                 </ul>
             </div>
 
