e3e0b81c430bb71b81c09f8c07d894d2896dffbb
Priyanshu Nandan <priyanshunandan_ee22b17_49@dtu.ac.in>
2025-04-21 03:36:01 +0530

    Finished the Static version of the website. Without responsiveness

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,6 +15,11 @@ import Me from '../images/me.png'
 import Train from '../images/train-2.png'
 import Toy from '../images/toy.png'
 import Think from '../images/think.png'
+import BarImg from '../images/charabar.png'
+import Sitting from '../images/sitting-chara.png'
+import Constellation from '../images/constellation.png';
+import WizPeek from '../images/wizpeek.png';
+import Footer from '../components/Footer';
 import { Link } from 'react-router-dom'
 export default function About() {
   return (
@@ -62,8 +67,8 @@ export default function About() {
                 <div className='justify-start text-black text-2xl font-semibold font-sora leading-loose'>Priyanshu Nandan</div>
                 <div className='self-stretch justify-start text-black text-lg font-normal font-rubik leading-7'>(CEO of Anime)</div>
             </div>
-              <div className='self-stretch justify-start text-black text-xl font-normal font-rubik'>Mob: 9599144299</div>
-              <div className='self-stretch justify-start text-black text-xl font-normal font-rubik'>lazywiz609@gmail.com</div>
+              <div className='self-stretch justify-start text-black text-[16px] font-normal font-rubik'>Mob: 9599144299</div>
+              <div className='self-stretch justify-start text-black text-[16px] font-normal font-rubik'>lazywiz609@gmail.com</div>
               <div className='flex justify-start items-center gap-4 mt-2'>
                 <a href="https://github.com/LazyWiz609" target="_blank" rel="noopener noreferrer">
                   <div className="flex p-[10px] justify-center items-center border-[2.612px] border-[#3E496A] bg-[#292F42] hover:border-[#292F42] hover:bg-[#3E496A] active:border-[#292F42] active:bg-[#1B1F23]">
@@ -100,15 +105,15 @@ export default function About() {
           <div className='flex justify-center gap-32'>
             <div className='flex items-center gap-4'>
               <img src={Spotify} alt='spotify'/>
-              <div className="justify-start text-black text-[16px] font-semibold font-['Sora'] leading-7">2970+ Minutes Listened</div>
+              <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">2970+ Minutes Listened</div>
             </div>
             <div className='flex items-center gap-4'>
               <img src={Luffy} alt='luffy'/>
-              <div className="justify-start text-black text-[16px] font-semibold font-['Sora'] leading-7">265+ Anime Watched</div>
+              <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">265+ Anime Watched</div>
             </div>
             <div className='flex items-center gap-4'>
               <img src={VsCode} alt='vscode'/>
-              <div className="justify-start text-black text-[16px] font-semibold font-['Sora'] leading-7">1M+ Lines of Code Written</div>
+              <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">1M+ Lines of Code Written</div>
             </div>
           </div>
             <span className="text-black text-3xl font-semibold font-sora leading-9 text-center">"I love to make things that people enjoy more than just visiting. Experiences that can be felt than seen."</span>
@@ -149,6 +154,126 @@ export default function About() {
       <img src={Think} alt="thinking" className="h-auto" />
     </div>
     </div>
-    </div>
+
+    {/* Section - 4 */}
+      <div className='relative flex items-center justify-center min-h-screen min-w-full rounded-tl-[68px] rounded-tr-[68px] pt-[100px] py-[80px] bg-white gap-24'>
+        <div className='relative self-stretch inline-flex flex-col justify-start items-start gap-8'>
+          <div className='self-stretch justify-start text-black text-4xl font-bold font-sora leading-[56px]'>Skills</div>
+            <div className='inline-flex flex-col justify-start items-start gap-4'>
+              <div className='self-stretch justify-start text-black text-[24px] font-semibold font-sora leading-9'>Design</div>
+              <div className='inline-flex justify-start items-end gap-12'>
+                <img src='/icons/ps.png' alt="photoshop" className='scale-75'/>
+                <img src='/icons/figma.png' alt="figma" className='scale-75'/>
+                <img src='/icons/ai.png' alt='illustrator' className='scale-75'/>
+                <img src='/icons/canva.png' alt='canva' className='scale-75'/>
+                <img src='/icons/blender.png' alt='blender' className='scale-75'/>
+                <img src='/icons/lightroom.png' alt='lightroom' className='scale-75'/>
+              </div>
+            </div>
+            <div className='inline-flex flex-col justify-start items-start gap-4'>
+              <div className="self-stretch justify-start text-black text-[24px] font-semibold font-sora leading-9">Coding</div>
+                <div className='inline-flex justify-start items-end gap-12 flex-wrap content-end'>
+                  <img src='/icons/c++.png' alt='c++' className='scale-75'/>
+                  <img src='/icons/html5.png' alt='html' className='scale-75'/>
+                  <img src='/icons/css.svg' alt='css' className='scale-75'/>
+                  <img src='/icons/js.png' alt='js' className='scale-75'/>
+                  <img src='/icons/python.png' alt='python' className='scale-75'/>
+                  <img src='/icons/java.png' alt='java' className='scale-75'/>
+                </div>
+                <div className='inline-flex justify-start items-end gap-12 '>
+                  <img src='/icons/mongo.png' alt='mongo' className='scale-75'/>
+                  <img src='/icons/express.png' alt='express' className='scale-75'/>
+                  <img src='/icons/react.png' alt='raect' className='scale-75'/>
+                  <img src='/icons/mysql.png' alt='mysql' className='scale-75'/>
+                  <img src='/icons/nodejs.png' alt='nodejs' className='scale-75'/>
+                  <img src='/icons/php.png' alt='php' className='scale-75'/>
+                </div>
+            </div>
+            <div className='self-stretch relative flex flex-col justify-start items-start gap-4 z-0'>
+            <div className="self-stretch justify-start text-black text-[24px] font-semibold font-sora leading-9">Extras</div>
+              <div className='self-stretch inline-flex justify-start items-end gap-14'>
+                <div className='inline-flex flex-col justify-start items-start gap-4'>
+                  <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">Leadership:  ⭐⭐⭐⭐</div>
+                  <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">Story Telling: ⭐⭐⭐⭐</div>
+                  <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">Growth Mindset: ⭐⭐⭐⭐⭐</div>
+                  <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">Problem Solving: ⭐⭐⭐⭐⭐</div>
+                  <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">Adaptability: ⭐⭐⭐⭐⭐</div>
+                </div>
+                <div className='inline-flex flex-col justify-start items-start gap-4'>
+                  <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">Teamwork: ⭐⭐⭐⭐</div>
+                  <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">Communication: ⭐⭐⭐⭐</div>
+                  <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">Presentation: ⭐⭐⭐⭐</div>
+                  <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">Emotional Intelligence: ⭐⭐⭐⭐⭐</div>
+                  <div className="justify-start text-black text-[16px] font-semibold font-sora leading-7">Time Management: ⭐⭐⭐⭐⭐</div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <img src={BarImg} alt='chara-bar'/>
+      </div>
+
+{/* Many More to Come */}
+
+      <div className='relative flex flex-col items-center justify-start min-h-screen py-32 overflow-hidden'>
+        <div className='flex flex-col absolute top-1/5 items-center gap-2'>
+          <div class="relative inline-block">
+      {/* <!-- Glowing text behind --> */}
+            <div class="absolute inset-0 text-white text-5xl font-bold font-sora leading-[56px] blur-sm opacity-60">
+              One Star at a Time...
+            </div>
+            
+            {/* <!-- Main visible text --> */}
+            <div class="relative text-white text-5xl font-bold font-sora leading-[56px]">
+              One Star at a Time...
+            </div>
+          </div>
+
+          <div className='relative'>
+            <div className='absolute inset-0 text-white text-4xl font-normal font-sora leading-10 blur-sm opacity-60'>
+            Still Expanding
+            </div>
+            <div className='relative text-white text-4xl font-normal font-sora leading-10'>
+            Still Expanding
+            </div>
+          </div>
+        </div>
+          <div className='absolute bottom-0'>
+            <div className='absolute blur-sm'>
+                <img src={Sitting} alt='sitting-chara' className='w-[265.5px] h-[356.25px]'/>
+            </div>
+            <div className='relative'>
+                <img src={Sitting} alt='sitting-chara' className='w-[265.5px] h-[356.25px]'/>
+            </div>
+          </div>
+          <img src="/constellations/const1.png" className="absolute top-[40%] right-[20%] w-[250px] opacity-70" />
+          <img src="/constellations/const2.png" className="absolute -top-[10%] -right-[5%] w-[320px] opacity-60" />
+          <img src="/constellations/const2.png" className="absolute -top-[6%] left-[15%] w-[320px] opacity-60" />
+          <img src="/constellations/const3.png" className="absolute -bottom-[15%] -left-[8%] w-[290px] opacity-50" />
+          <img src="/constellations/const3.png" className="absolute -top-[15%] -left-[8%] w-[290px] opacity-50" />
+          <img src="/constellations/const4.png" className="absolute -bottom-[10%] right-[5%] w-[240px] opacity-75" />
+          <img src="/constellations/const5.png" className="absolute top-[50%] left-[12%] w-[265px] opacity-40" />
+          <img src="/constellations/const5.png" className="absolute top-[30%] -right-[10%] w-[265px] opacity-40" />
+      </div>
+
+      {/* Contact Me */}
+      <div className='flex w-full px-32 py-64 relative bg-[#1B1F23] flex-col justify-center items-center gap-16 overflow-hidden' style={{ boxShadow: 'inset 0 0 72px 10px rgba(0, 0, 0, 0.25)',}}>
+        <div className=' relative z-10 justify-start text-white text-[40px] font-bold font-sora leading-[44px]'>Wanna Collab, Hire or Just Chat ?</div>
+          <div className=' relative flex items-center justify-center gap-8 z-10'>
+            <ExploreButton name={"Hire Me Now !"} iconName={"arrow-redo-outline"}/>
+            <PurpleButton name={"Let's Collaborate"} iconName={"arrow-redo-outline"}/>
+          </div>
+          <img
+      src={Constellation}
+      alt='background'
+      className='fixed top-0 left-0 w-full h-full object-cover opacity-[0.02] z-0 pointer-events-none'
+      />
+      </div>
+
+      {/* Footer */}
+      <div className='relative w-full'>
+        <img src={WizPeek} alt='Wiz Peek' className="w-64 h-64 absolute right-0 bottom-43 z-20" />
+          <Footer />
+        </div>
+  </div>
   )
 }
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Flying from '../images/flying.png'
+import PurpleButton from '../components/PurpleButton'
+import ExploreButton from '../components/ExploreButton'
 
 export default function Blog() {
   return (
     <div>
-      
+      <div className='flex flex-col justify-center items-center gap-6 min-h-screen'>
+
+        <div className='relative'>
+            <div className='absolute inset-0 text-center justify-start text-white text-5xl font-bold font-sora leading-[56px] blur-sm opacity-60'>
+            Coming Soon..
+            </div>
+            <div className='relative text-center justify-start text-white text-5xl font-bold font-sora leading-[56px]'>
+            Coming Soon..
+            </div>
+          </div>
+          <div className='relative'>
+            <div className='absolute inset-0 text-center justify-start text-white text-4xl font-normal font-sora leading-10 blur-sm opacity-60'>
+            Wanna play a sweet game ?
+            </div>
+            <div className='relative text-center justify-start text-white text-4xl font-normal font-sora leading-10'>
+            Wanna play a sweet game ?
+            </div>
+          </div>
+          <PurpleButton name={"Yeah Sure !"} iconName={"game-controller-outline"}/>
+      </div>
     </div>
   )
 }
